6a154f4bc79a415f3286abc6e6501304dcffc8b1
Mark <remarkablemark@users.noreply.github.com>
2022-12-25 19:07:48 -0500

    perf(constants): replace render with replit
    https://replit.com/@remarkablemark/CORSmirror

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-export const CORSMIRROR_URL = 'https://corsmirror.onrender.com';
+// https://corsmirror.onrender.com
+export const CORSMIRROR_URL = 'https://corsmirror.remarkablemark.repl.co';
 export const NPM_PACKAGE_URL = 'https://www.npmjs.com/package';
 const NPM_REGISTRY_URL = 'https://registry.npmjs.com';
 export const REQUEST_URL = `${CORSMIRROR_URL}/v1/cors?url=${NPM_REGISTRY_URL}`;
