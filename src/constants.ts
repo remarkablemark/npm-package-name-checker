57704d4bdb9addf78aca81af75ef7a45dac59651
Mark <mark@remarkablemark.org>
2023-05-23 00:55:33 -0400

    fix(constants): fix corsmirror domain

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const NPM_PACKAGE_URL = 'https://www.npmjs.com/package';
 const NPM_REGISTRY_URL = 'https://registry.npmjs.com';
-export const REQUEST_URL = `https://corsmirror.com/v1?url=${NPM_REGISTRY_URL}`;
+export const REQUEST_URL = `https://corsmirror.pages.dev/v1?url=${NPM_REGISTRY_URL}`;
 export const DEBOUNCE_DELAY_IN_MILLISECONDS = 300;
